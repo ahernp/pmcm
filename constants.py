19b047f5208fc3740632e67e68f7b5583d30f0f4
Paul Ahern <ahernp@ahernp.com>
2026-07-03 21:38:18 +0100

    Increase history from 20 to 30 pages

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-HISTORY_LENGTH = 20
+HISTORY_LENGTH = 30
 HISTORY_FILE_PATH = Path("data") / "history.txt"
 MEDIA_ROOT_PATH = Path("media")
 PAGES_PATH = Path("data") / "pages"
